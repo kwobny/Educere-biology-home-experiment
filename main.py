42896f37b1b315ba57c456b817513f15d8cd192d
Yeongjin Kwon <yeongjinkwon@gmail.com>
2021-08-23 19:23:00 +0000

    Finish prompt for args function

--- main.py
+++ main.py
@@ -11,11 +11,14 @@
 
 import sys
 import json
-import itertools
+import os
+
+def clearScreen():
+    os.system('clear')
 
 # This function prompts the user for the sorting algorithm to use, and the number of times to sort.
 # Parameters: A list of tuples. The tuple's first element should be the name (string) of the sorting algorithm. The second element should be the associated sorting function/object
-# Returns a tuple containing the sorting algorithm to use (function/object) and the number of times.
+# Returns a tuple containing the sorting algorithm to use (function/object) and the number of times (in that order).
 def promptForArgs(algorithms):
     print("Which sorting algorithm?: \n")
     
@@ -27,14 +30,29 @@ def promptForArgs(algorithms):
     while True:
         try:
             algToUse = algorithms[int(userResponse)][1]
+            break
         except (IndexError, ValueError) as err:
             print("Invalid input. Error:")
             print(err)
             userResponse = input("\nTry again:\n")
-            continue;
-        break;
+    
+    clearScreen()
+    userResponse = input("How many times to run?:\n")
+    while True:
+        try:
+            timesToRun = int(userResponse)
+        except ValueError:
+            userResponse = input("Input is not a number. Try again:\n")
+            continue
+        if timesToRun < 0:
+            userResponse = input("Input cannot be less than 0. Try again:\n")
+        else:
+            break
+    
+    return algToUse, timesToRun
 
 promptForArgs([('lol', object())])
+quit()
 
 if len(sys.argv) < 2:
     raise RuntimeError("Not enough arguments provided.")
